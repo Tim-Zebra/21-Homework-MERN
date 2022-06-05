d70804fc3913d9d3c92e61031faa1f8dfa64c1e3
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-05 12:46:35 -0500

    Added guess code for login and create user on client

--- my-app/client/src/utils/mutations.js
+++ my-app/client/src/utils/mutations.js
@@ -2,12 +2,29 @@ import { gql } from '@apollo/client';
 
 // Creates user
 export const CREATE_USER = gql`
-
+mutation createUser($username: String!, $email: String!, $password: String!) {
+  createUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+      email
+    }
+  }
+}
 `;
 
 // logs in user
 export const LOGIN_USER = gql`
-
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
 `;
 
 // saves books
