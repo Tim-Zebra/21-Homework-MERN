f6f4062d352be3b6a7d66d3fc22a7b585f060a99
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-04 21:00:29 -0500

    added imports for resolver

--- my-app/server/schemas/resolvers.js
+++ my-app/server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
