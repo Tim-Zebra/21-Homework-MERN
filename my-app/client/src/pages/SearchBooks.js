9fb6f6a1e1effa40e8c75c7098af6260a2c7fec3
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-07 15:13:54 -0500

    added book to save guess code

--- my-app/client/src/pages/SearchBooks.js
+++ my-app/client/src/pages/SearchBooks.js
@@ -19,20 +19,7 @@ const SearchBooks = () => {
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
   // Adds a book
-  const [saveBook, { error }] = useMutation(SAVE_BOOK, {
-    update(cache, { data: { saveBook } }) {
-      try {
-        const { savedBooks } = cache.readQuery({ query: QUERY_USER });
-
-        cache.writeQuery({
-          query: QUERY_USER,
-          data: { savedBooks: [saveBook, ...savedBooks] },
-        });
-      } catch (e) {
-        console.error(e);
-      }
-    },
-  });
+  const [saveBook, { error }] = useMutation(SAVE_BOOK)
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -85,16 +72,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
       const { book } = await saveBook({
-        variables: {
-          thoughtText,
-          thoughtAuthor: Auth.getProfile().data.username,
-        },
+        variables: {input: { ...bookToSave } }
       });
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
