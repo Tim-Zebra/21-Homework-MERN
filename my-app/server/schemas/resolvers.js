9f4554aacbbe35c612c6ddbfbb343164c163329a
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-08 01:36:40 -0500

    reverted code to base, copied two working files to try and trouble shoot

--- my-app/server/schemas/resolvers.js
+++ my-app/server/schemas/resolvers.js
@@ -3,64 +3,60 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            console.log(context.user)
-            if (context.user) {
-                return User.findOne({ _id: context.user._id }).populate('savedBooks');
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
+  Query: {
+    // Finds user based off the jwt context
+    me: async (parent, args, context) => {
+      if(context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('You are not logged in!');
     },
+  },
+  Mutation: {
+    addUser: async (parent, {username, email, password}) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
 
-    Mutation: {
-        login: async (parent, { email, username, password }) => {
-            const user = await User.findOne({
-                $or: [{ username }, { email }]
-            })
-
-            console.log(user)
-
-            if (!user) {
-                throw new AuthenticationError('No user found with this username or email');
-            }
-
-            const correctPw = await user.isCorrectPassword(password);
-
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect password');
-            }
-
-            const token = signToken(user);
-
-            return { token, user };
-        },
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-        },
-        saveBook: async (parent, { input }, context) => {
-            if (context.user) {
-                return await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $addToSet: { savedBooks: input } },
-                    { new: true },
-                );
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
-        removeBook: async (parent, { bookId }, context) => {
-            if (context.user) {
-                return await User.findOneAndUpdate(
-                    { _id: context.user._id },
-                    { $pull: { savedBooks: { bookId } } },
-                    { new: true },
-                );
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        }
+      return { token, user };
     },
-}
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user with this email found!');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect password!');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      console.log('This happened');
+      console.log('SAVED BOOK ARGUMENTS', args.input);
+      if(context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args } },
+          { new: true, runValidators: true }
+        );
+      }
+      throw new AuthenticationError('Could NOT add book!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if(context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true },
+        );
+      }
+    },
+  },
+};
 
 module.exports = resolvers;
