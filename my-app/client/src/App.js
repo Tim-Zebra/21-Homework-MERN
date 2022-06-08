54a1e47e0028d4de5c900f67a7a857c35ffd0ec9
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-08 01:29:41 -0500

    reverted app.js. everything funtioning as intended

--- my-app/client/src/App.js
+++ my-app/client/src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { 
-  ApolloClient, 
-  ApolloProvider, 
-  InMemoryCache, 
-  createHttpLink, 
-} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -15,9 +11,11 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token');
-  
+  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -53,7 +51,7 @@ function App() {
           </Routes>
         </>
       </Router>
-    </ApolloProvider>
+    </ApolloProvider> 
   );
 }
 
