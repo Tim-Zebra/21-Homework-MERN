fd3fd26eb4802928e2b35b46a434edfbef83a35a
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-08 15:12:45 -0500

    re added secret key

--- my-app/server/utils/auth.js
+++ my-app/server/utils/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // set token secret and expiration date
-const secret = "mysecretttttttt";
+const secret = process.env.JWT_SECRET_KEY;
 const expiration = '2h';
 
 module.exports = {
