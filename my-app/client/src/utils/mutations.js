94610d3569e8b62a3a9e0aa0288a32c6472d021b
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-08 02:11:08 -0500

    needed to chagne STRING! to ID! Assuming this has to do when a conversion from mutation to query from ID to string. But if it thinks it's passed a string and it just gets numbers that'll cause probs....Hmmm maybe .json has something to do with it

--- my-app/client/src/utils/mutations.js
+++ my-app/client/src/utils/mutations.js
@@ -1,62 +1,66 @@
 import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
-    }
-`
-
+// Creates user
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+mutation addUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
     }
+  }
+}
 `;
 
-export const SAVE_BOOK = gql`
-    mutation saveBook($input: BookInput!) {
-        saveBook(input: $input) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
-        }
+// logs in user
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
-`
+  }
+`;
 
-export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-            }
-        }
+// saves a book
+export const SAVE_BOOK = gql`
+mutation saveBook($input: BookInput!) {
+  saveBook(input: $input) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      image
+      link
+      title
     }
-`
+  }
+}
+`;
+
+// deletes a book
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!) {
+  removeBook(bookId: $bookId) {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      image
+      title
+    }
+  }
+}
+`;
