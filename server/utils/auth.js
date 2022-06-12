63a95005d5e3cc4275080f3f83aa91e2b3ae2afa
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-12 17:39:23 -0500

    moved and updated file locations so heroku can deploy correctly.

---
+++ server/utils/auth.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// set token secret and expiration date
+const secret = process.env.JWT_SECRET_KEY;
+const expiration = '2h';
+
+module.exports = {
+  // function for our authenticated routes
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    // declines if invalid token
+    if (!token) {
+      return req;
+    }
+
+    // Verifies token
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ email, username, _id }) {
+    const payload = { email, username, _id };
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
