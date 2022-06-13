ec3a6fe179e82b5db75d92ff877614f0a50237f1
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-06-12 20:41:52 -0500

    removed option to view server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 // Server/App/Port variables
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
